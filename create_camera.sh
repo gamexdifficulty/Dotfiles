824684c43995dd7b902b7012cd5a0cbd3bc09d42
gamexdifficulty <106142331+gamexdifficulty@users.noreply.github.com>
2025-05-17 01:57:50 +0200

    Update create_camera.sh

--- create_camera.sh
+++ create_camera.sh
@@ -3,7 +3,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install libcamera-tools docker.io ffmpeg -y
 echo '#!/bin/bash
 docker run --rm -d -p 8554:8554 aler9/rtsp-simple-server
-libcamera-vid -t 0 --framerate 24 --width 1920 --height 1080 --inline --buffer-count 4 --denoise cdn_off -o - | ffmpeg -re -i - -c:v libx264 -preset ultrafast -tune zerolatency -bufsize 2M -maxrate 2M -f rtsp -rtsp_transport tcp rtsp://192.168.2.163:8554/live/stream' >> start_cam.sh
+libcamera-vid -t 0 --framerate 24 --width 1920 --height 1080 --inline --buffer-count 4 --denoise cdn_off -o - | ffmpeg -re -i - -c:v libx264 -preset ultrafast -tune zerolatency -bufsize 2M -maxrate 2M -f rtsp -rtsp_transport tcp rtsp://127.0.0.1:8554/live/stream' >> start_cam.sh
 sudo tee /etc/systemd/system/start_cam.service > /dev/null <<EOF
 [Unit]
 Description=Start sec cam
